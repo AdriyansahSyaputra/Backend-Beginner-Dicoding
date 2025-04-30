35d30628ba73ab855ec590ecc7c32d08c0329bce
Adriyansah Syaputra <adriansyahputra2626@gmail.com>
2025-04-30 19:33:15 +0700

    created CRUD notes with HAPI, and make id with nanoid

--- NOTES-APP-BACK-END/src/server.js
+++ NOTES-APP-BACK-END/src/server.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+const Hapi = require("@hapi/hapi");
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5050,
+    host: "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  // Daftarkan routes
+  server.route(routes);
+  
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
